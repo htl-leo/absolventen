d6589008b8177ad34de6ddc9eef99e1d2f356a4b
gkoe <gkoe@users.noreply.github.com>
2025-11-04 14:53:34 +0100

    Impressum: oesterreichisches Impressum hinzugefuegt und im Footer verlinkt

--- Iot_MudDataGrid/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Iot_MudDataGrid/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36efcc185d049cf72ca6325fa55e8eb305cd7d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18a76b30b250dbda618fd9e723c12b5d75392eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
